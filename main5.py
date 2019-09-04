dfafd5ce809cf2f90e1b5a322dd2394ef57c0f1d
Michael <michael.burks77@gmail.com>
2019-09-04 16:58:22 -0400

    Edited License, readme, and completed assignment

--- main5.py
+++ main5.py
@@ -4,7 +4,7 @@ import utils, open_color, arcade
 
 utils.check_version((3,7))
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 800 # Defining the screen peramiters
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Smiley Face Example"
 
@@ -17,7 +17,7 @@ class Faces(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         # Show the mouse cursor
-        self.set_mouse_visible(True)
+        self.set_mouse_visible(True) #this is maybe defining the centerpoint as the mouse position
 
         self.x = SCREEN_WIDTH / 2
         self.y = SCREEN_HEIGHT / 2
@@ -26,7 +26,7 @@ class Faces(arcade.Window):
 
     def on_draw(self):
         """ Draw the face """
-        arcade.start_render()
+        arcade.start_render() #Lines 30-35 create the smiley face with the centerpoint as the mouse position
         face_x,face_y = (self.x,self.y)
         smile_x,smile_y = (face_x + 0,face_y - 10)
         eye1_x,eye1_y = (face_x - 30,face_y + 20) 
@@ -34,7 +34,7 @@ class Faces(arcade.Window):
         catch1_x,catch1_y = (face_x - 25,face_y + 25) 
         catch2_x,catch2_y = (face_x + 35,face_y + 25) 
 
-        arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3)
+        arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3) #I believe thesse lines are animating the smiley in order to allow it to move with the mouse cursor
         arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
         arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black)
         arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
@@ -43,7 +43,7 @@ class Faces(arcade.Window):
         arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4)
 
 
-    def on_mouse_motion(self, x, y, dx, dy):
+    def on_mouse_motion(self, x, y, dx, dy): # these 4 lines are defining the variables as being the mouse cursor
         """ Handle Mouse Motion """
         self.x = x
         self.y = y
